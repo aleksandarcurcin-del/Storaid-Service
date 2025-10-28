a656b70db62a6baf77eceeb888fdb2fa370b2865
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-28 12:50:03 +0100

    added cards component

---
+++ src/components/Cards.jsx
@@ -0,0 +1,27 @@
+import './Cards.css'
+
+function Cards({ date, title, text, imageURL }) {
+  return (
+    <div className="cards-inner">
+      <div className="card-box">
+        <div className="cardbox-inner">
+          <img className="blog-image" src={imageURL} />
+        </div>
+        <div className="card-content">
+          <div className="calendar">
+            <img className="calendar-icon" src="/images/calendaricon.svg" alt="" /> <p>{date}</p>
+          </div>
+          <div className="card-text">
+            <h6>{title}</h6>
+            <p>{text}</p>
+          </div>
+          <div className="card-link">
+            <a className="read-more" href="/">Read More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cards
