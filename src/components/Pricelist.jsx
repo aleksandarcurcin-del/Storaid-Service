715ccbe6077f2540faa9ebb81eab1746bb128e88
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-28 17:48:41 +0100

    Added hovereffect to checkmarks

--- src/components/Pricelist.jsx
+++ src/components/Pricelist.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import './Pricelist.css'
 import Buttons from "./Buttons";
-
+import checkmarkGreen from '/images/checkicon.svg'
+import checkmarkyellow from '/images/yellowcheckicon.svg'
 
 
 function Pricelist({ title, price }) {
+
+    const [isHovered, setIsHovered] = useState(false)
+
+
   return (
-        <div className="pricing-card">
+        <div className="pricing-card"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+        >
             <h5>{title}</h5>
             <div className="prices">
                 <img className="dollar-icon" src="/images/dollaricon.svg" alt="" />
@@ -17,19 +26,19 @@ function Pricelist({ title, price }) {
 
             <div className="pricing-lower">
                 <div className="pricing-check">
-                    <img className="check-icon" src="/images/checkicon.svg" alt="" /> <p>Nam nec ipsum in dolor</p>
+                    <img className="check-icon" src={isHovered ? checkmarkyellow : checkmarkGreen} alt="" /> <p>Nam nec ipsum in dolor</p>
                 </div>
                 <div className="pricing-check">
-                    <img className="check-icon" src="/images/checkicon.svg" alt="" /> <p>Fusce nec ligula ut arcu</p>
+                    <img className="check-icon" src={isHovered ? checkmarkyellow : checkmarkGreen} alt="" /> <p>Fusce nec ligula ut arcu</p>
                 </div>
                 <div className="pricing-check">
-                    <img className="check-icon" src="/images/checkicon.svg" alt="" /> <p>Aliquam pulvinar arcu in</p>
+                    <img className="check-icon" src={isHovered ? checkmarkyellow : checkmarkGreen} alt="" /> <p>Aliquam pulvinar arcu in</p>
                 </div>
                 <div className="pricing-check">
-                    <img className="check-icon" src="/images/checkicon.svg" alt="" /> <p>Duis gravida enim porta</p>
+                    <img className="check-icon" src={isHovered ? checkmarkyellow : checkmarkGreen} alt="" /> <p>Duis gravida enim porta</p>
                 </div>
                 <div className="pricing-check">
-                    <img className="check-icon" src="/images/checkicon.svg" alt="" /> <p>Etiam eget libero non ligula</p>
+                    <img className="check-icon" src={isHovered ? checkmarkyellow : checkmarkGreen} alt="" /> <p>Etiam eget libero non ligula</p>
                 </div>
                 <div className="rent-btn">
                     <Buttons text="Rent Now" />
