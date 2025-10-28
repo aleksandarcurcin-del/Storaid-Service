b2107d42e46c2efeb7d0cce7903e2b853e739652
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-28 11:58:30 +0100

    added testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Brands from "./components/Brands"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import Ourservices from "./components/Ourservices"
+import Testimonials from "./components/Testimonials"
 
 function App() {
 
@@ -19,6 +20,7 @@ function App() {
             <About />
             <Brands />
             <Ourservices />
+            <Testimonials />
           </main>
 
 
