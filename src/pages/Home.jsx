b05dca0b79ba7e846c5028a893539497e21517ec
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-29 11:52:27 +0100

    adjusted links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
+import Hero from '../components/Hero'
+import About from '../components/About'
+import Brands from '../components/Brands'
+import Ourservices from '../components/Ourservices'
+import Testimonials from '../components/Testimonials'
+import Choose from '../components/Choose'
+import Pricing from '../components/Pricing'
+import Book from '../components/Book'
+import Blog from '../components/Blog'
+import Subscribe from '../components/Subscribe'
+
+
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Hero />
+      <About />
+      <Brands />
+      <Ourservices />
+      <Testimonials />
+      <Choose />
+      <Pricing />
+      <Book />
+      <Blog />
+      <Subscribe />
+    </>
   )
 }
 
