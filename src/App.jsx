966b5eeb5ac05e2d3e5db88d645bb5208c1d0534
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-29 11:08:35 +0100

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
-
-import About from "./components/About"
-import Blog from "./components/Blog"
-import Book from "./components/Book"
-import Brands from "./components/Brands"
-import Choose from "./components/Choose"
-import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Aboutus from "./pages/Aboutus"
+import Services from "./pages/Services"
+import Contactus from "./pages/Contactus"
 import Header from "./components/Header"
-import Hero from "./components/Hero"
-import Ourservices from "./components/Ourservices"
-import Pricing from "./components/Pricing"
-import Subscribe from "./components/Subscribe"
-import Testimonials from "./components/Testimonials"
+import Footer from "./components/Footer"
+
+
+
 
 function App() {
 
   return (
-    <>
-      
-      
-        <div className="wrapper">
-          <Header />
-
-          <main>
-            <Hero />
-            <About />
-            <Brands />
-            <Ourservices />
-            <Testimonials />
-            <Choose />
-            <Pricing />
-            <Book />
-            <Blog />
-            <Subscribe />
-          </main>
-
-
-          <Footer />
-        </div>
-
-    </>
-
+    <BrowserRouter>
+      <div className="wrapper">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutus" element={<Aboutus /> } />
+            <Route path="/services" element={<Services /> } />
+            <Route path="/contactus" element={<Contactus /> } />
+          </Routes>          
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
   )
 }
 
