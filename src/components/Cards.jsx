6be7caec8daea4ef6dabcef45486f19a365847cb
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-30 12:07:35 +0100

    adjusted content

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,34 +1,28 @@
 import './Cards.css'
-import { Link } from 'react-router-dom'
 
-function Cards({ item }) {
+function Cards({ date, title, text, imageURL }) {
 
-
-
+  
   return (
-    <Link to={`/cards-api/${item.id}`}>
-      <div className="item">
-        <div className="cards-inner">
-          <div className="card-box">
-            <div className="cardbox-inner">
-              <img className="blog-image" src={item.imageURL} />
-            </div>
-            <div className="card-content">
-              <div className="calendar">
-                <img className="calendar-icon" src="/images/calendaricon.svg" alt="" /> <p>{item.date}</p>
-              </div>
-              <div className="card-text">
-                <h6>{item.title}</h6>
-                <p>{item.text}</p>
-              </div>
-              <div className="card-link">
-                <a className="read-more" href="/">Read More</a>
-              </div>
-            </div>
+    <div className="cards-inner">
+      <div className="card-box">
+        <div className="cardbox-inner">
+          <img className="blog-image" src={imageURL} />
+        </div>
+        <div className="card-content">
+          <div className="calendar">
+            <img className="calendar-icon" src="/images/calendaricon.svg" alt="" /> <p>{date}</p>
+          </div>
+          <div className="card-text">
+            <h6>{title}</h6>
+            <p>{text}</p>
+          </div>
+          <div className="card-link">
+            <a className="read-more" href="/">Read More</a>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
 
