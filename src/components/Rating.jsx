8f258b8f49666aa41fa14237b578b45a25d96157
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-30 13:49:46 +0100

    adjusted web-api

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,15 +4,14 @@ import './Testimonials.css'
 function Rating({ item }) {
   return (
         <div className="review">
-            <img className="stars" src="/images/fivestars.svg" alt="" />
+            <img className="stars" src={item.rating} alt="" />
             <p>{item.comment}</p>
             <div className="review-bottom">
-                <div className="circle"><img className="circle-inner" src={item.imageUrl} alt="" /></div>
+                <div className="circle"><img className="circle-inner" src={item.avatarUrl} alt="" /></div>
                 <div className="person">
                     <h6>{item.name}</h6>
                     <p className="customer">{item.companyName}</p>
                 </div>
-                <img src={item.rating} alt="" />
             </div>
         </div>
   )
