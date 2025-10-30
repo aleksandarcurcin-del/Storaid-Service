7efa864b41afee12a9019430bc3b55f40ef4ae08
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-30 15:10:33 +0100

    added new component with content for other pages than home

---
+++ src/components/Testimonialsextra.jsx
@@ -0,0 +1,40 @@
+import './Testimonials.css'
+import { useState, useEffect } from 'react'
+import Rating from './Rating'
+
+function Testimonialsextra() {
+    const [reviewItems, setReviewItems] = useState([])
+    
+    useEffect(() => {
+        (async () => {
+        const res = await fetch('https://win25-jsf-assignment.azurewebsites.net/api/testimonials')
+        const data = await res.json()
+        setReviewItems(data)
+        })()
+    }, [])
+
+
+
+  return (
+    <section className="testimonials">
+        <div className="testimonials-background">
+            <div className="container">
+                <div className="testimonials-flex">
+                    <div className="testimonials-text">
+                        <h4>Testimonials</h4>
+                        <h3>See What Our Client Have to Say</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie nisl sed dui lacinia gravida. Nulla quis nulla leo. Mauris ac blandit nisi non sodales augue. Phasellus eget elit gravida.</p>
+                    </div>
+                    <div className="testimonials-review">
+                        {
+                            reviewItems.map((item) => (<Rating key={item.id} item={item} />))
+                        }
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Testimonialsextra
