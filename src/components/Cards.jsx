45dc089df8ce2427fce62ae92507d1961916796e
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-10-30 15:24:56 +0100

    adjusted calendar icon on hover

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,17 +1,23 @@
 import './Cards.css'
+import { useState } from 'react';
+import calendarIconGreen from '/images/calendaricongreen.svg'
+import calendarIcon from '/images/calendaricon.svg'
 
 function Cards({ item }) {
-
+  const [isHovered, setIsHovered] = useState(false)
   
   return (
-    <div className="cards-inner">
+    <div className="cards-inner"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div className="card-box">
         <div className="cardbox-inner">
           <img className="blog-image" src={item.imageUrl} />
         </div>
         <div className="card-content">
           <div className="calendar">
-            <img className="calendar-icon" src="/images/calendaricon.svg" alt="" /> <p>{item.created}</p>
+            <img className="calendar-icon" src={isHovered ? calendarIconGreen : calendarIcon} alt="" /> <p>{item.created}</p>
           </div>
           <div className="card-text">
             <h6>{item.title}</h6>
