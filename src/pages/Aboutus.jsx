e2cdeb57c85b554b891e13e493e3e17bac516298
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-11-03 14:43:09 +0100

    adjusted about us page

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -4,6 +4,7 @@ import About from '../components/About'
 import Brands from '../components/Brands'
 import Testimonialsextra from '../components/Testimonialsextra'
 import Chooseextra from '../components/Chooseextra'
+import Titlecomponent from '../components/Titlecomponent'
 
 
 
@@ -11,7 +12,7 @@ function Aboutus() {
   return (
     <>
         
-        
+        <Titlecomponent Title="About Us" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
         <About />
         <Brands />  
         <Testimonialsextra />
