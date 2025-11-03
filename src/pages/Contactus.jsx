4e035ace55e9b5adeffd0064bf642865fde9775f
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-11-03 14:09:38 +0100

    added contact us page and its content

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Titlecomponent from '../components/Titlecomponent'
+import Ourservices from '../components/Ourservices'
+import Testimonialsextra from '../components/Testimonialsextra'
+import Faq from '../components/Faq'
+import Book from '../components/Book'
 
 function Contactus() {
   return (
-    <div>Contactus</div>
+    <>
+      <Titlecomponent Title="Contact Us" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
+      <Ourservices />
+      <Testimonialsextra />
+      <Faq />
+      <Book />
+    </>
   )
 }
 
