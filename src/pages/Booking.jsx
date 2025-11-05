eb4025191e1a52ca64e7af184bad296e3f8fec93
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-11-05 14:10:10 +0100

    added booking unit component to booking page

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Titlecomponent from '../components/Titlecomponent'
+import Bookingunit from '../components/Bookingunit'
 
 function Booking() {
     return (
         <>
             <Titlecomponent Title="Booking" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
-        
+            <Bookingunit />
         </>
     )
 }
