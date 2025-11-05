72e3c61b566191931724d71d36a2cf5c9bf5552c
Aleks <aleksandar.curcin@utb.ecutbildning.segit commit -m first>
2025-11-05 12:05:43 +0100

    adjusted contact us page

--- src/pages/Contactus.jsx
+++ src/pages/Contactus.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Titlecomponent from '../components/Titlecomponent'
-import Git from '../components/Git'
 import Faq from '../components/Faq'
 
 
@@ -8,7 +7,6 @@ function Contactus() {
   return (
     <>
       <Titlecomponent Title="Contact Us" Text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo." />
-      <Git />
       <Faq />
     </>
   )
